1dfa5937691100c4cf9d1e4ef687fd6babd723c6
Pedro Alegre Perrotta <pedroalegre29@gmail.com>
2023-02-23 21:56:01 -0300

    Add analytics

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import "./styles/globals.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage";
 import WelcomePage from "./components/Welcome/welcomePage";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <div className="main-app-container">
-      
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<WelcomePage />} />
-        <Route path="/english" element={<HomePage lang="english" />} />
-        <Route path="/spanish" element={<HomePage lang="spanish" />} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<WelcomePage />} />
+          <Route path="/english" element={<HomePage lang="english" />} />
+          <Route path="/spanish" element={<HomePage lang="spanish" />} />
+        </Routes>
+      </BrowserRouter>
+      <Analytics />
     </div>
   );
 }
